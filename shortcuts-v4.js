81600833b0c51b400cfcdb66105a21fd6e70a2da
Andrew Levin <andrewlevingit@hotmail.com>
2024-03-24 03:51:30 +0300

    2024-03-24 03:51:30 N

--- shortcuts-v4.js
+++ shortcuts-v4.js
@@ -1,5 +1,4 @@
 
-
 console.log('🎸🎸 shortcuts-v4.js');
 
 
@@ -13,158 +12,22 @@ function getYouTubeVideoID(url) {
     }
 }
 
+
+
+const menuShortcuts = {
+    'sh-row1': shortcutRow1,
+    'sh-row2': shortcutRow2,
+    'sh-row3': shortcutRow3,
+    'sh-row4': shortcutRow4,
+    'sh-ytb': shortcutYtb,
+    'sh-row-ytb': shortcutRowYtb,
+};
+
+const regexShInner = Object.keys(menuShortcuts).join('|');
+
+const regexShortcuts = new RegExp(`(\\[/?)(${regexShInner})\]`, "g");
+
 const getRange = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
-
-
-
-
-function constructRow(text){
-    const elem = document.createElement("div");
-    elem.className = "row mb-5";
-    elem.innerHTML = text;
-    return elem
-}
-
-function constructCol(text){
-    return `<div class="col col-12 col-md-6 col-lg-4 mb-5">${text}</div>`;
-}
-
-function constructTitle(text){
-    return `<h4 class="mt-2 mb-2">${text}</h4>`;
-}
-
-function constructUrl(url, text){
-    return `<a href="${url}">${text}</a>`;
-}
-
-function constructImg(src){
-    return `<img src=${src}>`;
-}
-
-function constructYoutubeThumbnail(src, url){
-    return `<div style="background-image: url(); background-size: cover; width: 100%; aspect-ratio: 1.8 / 1;">
-    <a href="${url}">
-        <span style="color: transparent; display: inline-block;">
-        <img src="${src}">
-      </span>
-    </a>
-</div>`;
-}
-
-function constructParagraph(text){
-    return `<p style="margin-bottom: 0.25rem;">${text}</p>`;
-}
-
-const urlPattern = /https?:\/\/[^\s<]+/g;
-
-function urlReplacer(html){
-    for(const url of html.match(urlPattern))
-        html = html.replace(url, constructUrl(url, url));
-    return html
-}
-
-
-function oneRowReplacer(html){
-    let text = '';
-    const rows = html.split('<br>');
-    if (!rows)
-        return html
-
-    const url = rows[0].trim().match(urlPattern)[0]; 
-    if (!url)
-        return html
-            
-    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?(.*&)?v=|embed\/)|youtu\.be\/)/;
-    if (!youtubeRegex.test(url))
-        return html
-    
-    const movieid = getYouTubeVideoID(url);
-    const imgSize = 'maxresdefault';
-    const imgSrc = `https://img.youtube.com/vi/${movieid}/${imgSize}.jpg`
-
-    if (rows.length > 2){
-        let descriptions = [];
-        for(const row of rows.slice(2))
-            descriptions.push(row.trim())
-
-        text  = constructParagraph(urlReplacer(descriptions.join('<br>'))).concat(text);
-    }
-        
-    
-    if (rows.length > 1)
-        text  = constructTitle(constructUrl(url, rows[1].trim())).concat(text);
-    
-    text  = constructYoutubeThumbnail(imgSrc, url).concat(text);
-
-    return text
-}
-
-function processingShortcuts(){
-    console.log('🎠 V3 - ProcessingShortcuts: -- V3');
-
-    let parent = document.getElementsByClassName("markdown-body")[0];
-
-    let shortcutBlocks = [];
-    let starting = -1;
-    for(const idx in parent.children){
-        const child =  parent.children[idx];
-        if (!child.innerHTML)
-            continue
-
-        if (child.innerHTML.includes('[shortcut]'))
-            starting = idx;
-
-        if (child.innerHTML.includes('[/\shortcut]'))
-            shortcutBlocks.push([Number(starting), Number(idx)]);
-    }
-
-    console.log('🎠 V3 - ProcessingShortcuts: - 2');
-
-    for(const block of shortcutBlocks){
-        console.log('🥎 Block: ', block);
-        let items = [];
-        const range = getRange(block[0], block[1]);
-        for(const idx of range){
-            console.log('🥎 idx: ', idx);
-            if (idx!=range.slice(0)[0] && idx!=range.slice(-1)[0])
-                items.push(constructCol(oneRowReplacer(parent.children[idx].innerHTML)));
-            parent.children[idx].innerHTML = '';
-        }
-        parent.children[block[0]].replaceWith(constructRow(items.join('')));
-    }
-
-
-
-    console.log('🐝 END ProcessingShortcuts: '); 
-}
-
-processingShortcuts();
-
-
-function blinkImagesReplacer(){
-    for(const img of document.getElementsByClassName("markdown-body")[0].querySelectorAll('img')){
-        img.onload = function() {
-            if (img.naturalWidth < 121){
-                let parts = img.src.split('/');
-                parts[parts.length - 1] = 'hqdefault.jpg';
-                img.src = parts.join('/');
-            }
-        };
-    }
-}
-
-blinkImagesReplacer();
-
-
-
-
-const shortcutsAll = [
-    'sh-row2',
-    'sh-row3',
-    'sh-row4',
-    'sh-ytb',
-];
-const regexShortcuts = new RegExp(`(\\[/?)(${shortcutsAll.join('|')})\]`, "g");
 
 function parsingShortcuts(htmlElement){
     let shortcutBlocks = [];
@@ -184,22 +47,12 @@ function parsingShortcuts(htmlElement){
             starting = idx;
             
         if (matches[1] == '[\/' )
-            shortcutBlocks.push([Number(starting), Number(idx), matches[2]]);  
+            shortcutBlocks.push({'startIndex': Number(starting), 'endIndex': Number(idx), 'name': matches[2]});  
     }
     return shortcutBlocks
 }
 
-function shortcutFunc(itmes){
-    console.log('🍱 shortcutFunc: ');
-    console.log('🍱 items: ', itmes);
-
-    const elem = document.createElement("h2");
-    elem.innerHTML = 'Header';
-    return elem
-
-}
-
-function processingShortcuts2(){
+function processingShortcuts(){
     console.log('🔮 V4 - processingShortcuts2: ');
 
     let parent = document.getElementsByClassName("markdown-body")[0];
@@ -208,22 +61,170 @@ function processingShortcuts2(){
     console.log('📚 shortcutBlocks: ', shortcutBlocks);
 
     for(const block of shortcutBlocks.reverse()){
-        console.log('🥎 Block: ', block);
         let innerChilds = [];
-        const range = getRange(block[0], block[1]);
+        const range = getRange(block.startIndex, block.endIndex);
         for(const idx of range.reverse()){
-            console.log('🥎 idx: ', idx);
-            if (idx!=block[0] && idx!=block[1])
+            if (idx!=block.startIndex && idx!=block.endIndex)
                 innerChilds.push(parent.children[idx]);
-            if (idx==block[0])
+            if (idx==block.startIndex)
                 continue
             parent.children[idx].remove();
         }
-        const constructedElem = shortcutFunc(innerChilds);
-        parent.children[block[0]].replaceWith(constructedElem);
+        if(! block.name in menuShortcuts)
+            continue
+        
+        const constructedElem = menuShortcuts[block.name](innerChilds);
+        parent.children[block.startIndex].replaceWith(constructedElem);
     }
 
-    console.log('🔮 Fina '); 
+    console.log('🔮 Final '); 
 }
 
-processingShortcuts2();
+
+
+
+
+function shortcutCol(childs, size=1){
+    const elem = document.createElement("div");
+    elem.className = "col col-12 col-md-12 col-lg-12  mb-5";
+    if(size==2)
+        elem.className = "col col-12 col-md-6 col-lg-6  mb-5";
+    if(size==3)
+        elem.className = "col col-12 col-md-6 col-lg-4  mb-5";
+    if(size==4)
+        elem.className = "col col-12 col-md-4 col-lg-3  mb-5";
+
+    for(const child of childs.reverse())
+        elem.append(child)
+    return elem
+}
+
+function shortcutRow1(childs){
+    const elem = document.createElement("div");
+    elem.className = "row mb-5";
+    for(const child of childs.reverse())
+        elem.append(shortcutCol([child], size=1))
+    return elem
+}
+
+function shortcutRow2(childs){
+    const elem = document.createElement("div");
+    elem.className = "row mb-5";
+    for(const child of childs.reverse())
+        elem.append(shortcutCol([child], size=2))
+    return elem
+}
+
+function shortcutRow3(childs){
+    const elem = document.createElement("div");
+    elem.className = "row mb-5";
+    for(const child of childs.reverse())
+        elem.append(shortcutCol([child], size=3))
+    return elem
+}
+
+function shortcutRow4(childs){
+    const elem = document.createElement("div");
+    elem.className = "row mb-5";
+    for(const child of childs.reverse())
+        elem.append(shortcutCol([child], size=4))
+    return elem
+}
+
+function checkUrl(url){
+    if (url.match(/https?:\/\/[^\s<]+/g))
+        return true
+    return false
+}
+
+function checkYoutubeUrl(url){
+    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?(.*&)?v=|embed\/)|youtu\.be\/)/;
+    if (youtubeRegex.test(url))
+        return true
+    return false
+}
+
+function tuningURL(text){
+    for(const url of text.match(/https?:\/\/[^\s<]+/g))
+        text = text.replace(url, `<a href="${url}">${url}</a>`);
+    return text
+}
+
+function shortcutYtb(childs){
+    const elem = document.createElement("div");
+    const paragraph = childs[0];
+
+    const rows = paragraph.innerHTML.split('<br>');
+    if (!rows.length)
+        return elem
+
+    const url = rows[0].trim();
+    if (!checkUrl(url))
+        return elem
+
+    if (!checkYoutubeUrl(url))
+        return elem
+
+    const movieID = getYouTubeVideoID(url);
+    const imgSize = 'maxresdefault';
+    const imgSrc = `https://img.youtube.com/vi/${movieID}/${imgSize}.jpg`
+
+    const elemImg = document.createElement("div");
+    elemImg.style = "background-image: url(); background-size: cover; width: 100%; aspect-ratio: 1.8 / 1;";
+    elemImg.innerHTML = `
+        <a href="${url}">
+            <span style="color: transparent; display: inline-block;">
+                <img src="${imgSrc}">
+            </span>
+        </a>`;
+    elem.append(elemImg);
+
+
+    if (rows.length < 2)
+        return elem
+
+    const title = rows[1].trim()
+    const titleElem = document.createElement("h4");    
+    titleElem.className = "mt-2 mb-2";
+    titleElem.innerHTML = `<a href="${url}">${title}</a>`;
+    elem.append(titleElem);
+
+
+    if (rows.length < 3)
+        return elem   
+
+    const descriptionElem = document.createElement("p");
+    descriptionElem.innerHTML = tuningURL(rows.slice(2).join('<br>'));
+    elem.append(descriptionElem);
+
+    return elem
+}
+
+
+function shortcutRowYtb(childs){
+    console.log('🍉 shortcutRow3Ytb: ');
+
+    const elem = document.createElement("div");
+    elem.className = "row mb-5";
+    for(const child of childs.reverse())
+        elem.append(shortcutCol([shortcutYtb([child])], size=3))
+    return elem
+}
+
+processingShortcuts();
+
+
+
+function blinkImagesReplacer(){
+    for(const img of document.getElementsByClassName("markdown-body")[0].querySelectorAll('img')){
+        img.onload = function() {
+            if (img.naturalWidth < 121){
+                let parts = img.src.split('/');
+                parts[parts.length - 1] = 'hqdefault.jpg';
+                img.src = parts.join('/');
+            }
+        };
+    }
+}
+
+blinkImagesReplacer();
